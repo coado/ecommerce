228fb3fa94c7ff3f664a603e30f6668ca27aa44c
Dawid Malecki <coadopl@gmail.com>
2021-04-17 14:45:03 +0200

    finishing sign in handle submit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,9 +16,11 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
         if (!userAuth) return;
 
+        // document reference
         const userRef = firestore.doc(`users/${userAuth.uid}`);
+        // reads document
         const snapShot = await userRef.get();   
-
+        // checking if user already exists.
         if (!snapShot.exists) {
                 const { displayName, email } = userAuth;
                 const createdAt = new Date();
